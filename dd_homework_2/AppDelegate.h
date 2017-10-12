6187d399acf382c9695bc16557be60ccb654a595
trishinSV <Sergey@MacBook-Pro-Sergej.local>
2017-10-13 00:05:50 +0300

    Initial Commit

---
+++ dd_homework_2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  dd_homework_2
+//
+//  Created by Сергей Тришин on 13.10.2017.
+//  Copyright © 2017 Сергей Тришин. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ dd_homework_2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  dd_homework_2
+//
+//  Created by Сергей Тришин on 13.10.2017.
+//  Copyright © 2017 Сергей Тришин. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
